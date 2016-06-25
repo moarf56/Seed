572c64a6df67e2c8cfee050cfd0c463532a15967
warezcmpt <warezcmpt@gmail.com>
2016-06-25 13:35:58 +0200

    Update znc.sh

--- znc/znc.sh
+++ znc/znc.sh
@@ -24,7 +24,7 @@ iptables -t filter -A INPUT -p tcp --dport $zncport -j ACCEPT
 iptables -t filter -A OUTPUT -p tcp --dport $zncport -j ACCEPT" >> /etc/init.d/firewall
 
 rm /etc/init.d/znc
-cp ~/JRabbitBox/znc/znc /etc/init.d/znc
+cp $cwd/znc/znc /etc/init.d/znc
 chmod 755 /etc/init.d/znc
 chmod +x /etc/init.d/znc
 
