08f286762b6908ca69cd15de8ec6e7981996a4b7
warezcmpt <warezcmpt@gmail.com>
2016-07-17 10:31:52 +0200

    Update wordpress.sh

--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -10,7 +10,7 @@ if [ -f "$file" ]
 then
         echo "MySQL found."
 else
-        apt-get -y --force-yes install mysql-server php7.0-mysql
+        apt-get -y --force-yes install mysql-server $PHPNAME-mysql
 fi
 
 cd /var/www/
